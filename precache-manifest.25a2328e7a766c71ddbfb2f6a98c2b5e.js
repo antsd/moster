725fe9c62fd3f642579057f06bed7e374a0ddfd3
anastasius <antsdevp@gmail.com>
2020-04-05 14:34:17 +0700

    Updates

---
+++ precache-manifest.25a2328e7a766c71ddbfb2f6a98c2b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abd6e30b0d3d50af47fb5ca9a1a7da5",
+    "url": "/moster/index.html"
+  },
+  {
+    "revision": "b33d80367cca0e44e7ee",
+    "url": "/moster/static/css/main.374eb0fe.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/moster/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moster/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33d80367cca0e44e7ee",
+    "url": "/moster/static/js/main.7b8c1ddd.chunk.js"
+  },
+  {
+    "revision": "d8de7b1a8b118b3919d7",
+    "url": "/moster/static/js/runtime-main.6140eb26.js"
+  }
+]);
